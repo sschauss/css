033a4ca6d8acfca50ede0471b783a1428018b809
AliAchttausend <39121769+AliAchttausend@users.noreply.github.com>
2018-05-16 18:22:38 +0200

    Update NYTimesHits.py

--- NYTimesHits.py
+++ NYTimesHits.py
@@ -19,3 +19,11 @@ for x in year:
     data= r.json()
     print(""+str(x)+": General "+str(data['response']['meta']['hits'])) 
 time.sleep(500)  
+"""
+Hey, wie ihr seht ist das ganze ziemlich hässlich geworden, aber wie gesagt, meine Coding-Skills sind ziemlich beschränkt.
+Ich hab als Ausgabe einfach nen kleinen String gemacht um mir die Werte mal direkt anzuschauen, dürfte aber kein Problem sein die irgendwie
+als CSV oder was auch immer zu speichern, je nachdem ob und wie wir damit weiterarbeiten wollen.
+Das einzige Problem bei der Funktionalität ist in den time.sleep() Funktionen, lass ich diese weg (bis auf die letzte) bekomm ich 2-3 Werte 
+ausgegeben und danach geht die ganze Kiste den Bach runter. Wieso ist mir nicht klar und ich war grade zu faul da noch errorhandler mit 
+einzubauen ;) Vielleicht wisst ihr ja wo da der Fehler liegt.
+"""
