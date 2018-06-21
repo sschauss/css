acf76d5fca5cf3950fb0b777ed79ffab92067ff0
Lukas Härtel <lukashaertel@gmail.com>
2018-06-21 23:13:48 +0200

    Implementation sentiment analysis and some fixes.

--- german/postag.py
+++ german/postag.py
@@ -1,11 +1,36 @@
+import warnings
+
 from pattern.text.de import parse
 
 from german.punkt import sentences_german
+
+
+def postag_german(sentence):
+    """
+    Returns for a sentence a list of tuples of (word, POS).
+    :param sentence: The sentence to tag.
+    :return: A list of tuples.
+    """
+    # Parse the sentence, target POS tags are STTS, do not do chunk parts and relations, use list of words and
+    # POS tags instead of complex tree. Base tokenization from library is applied but not relied on, as it just does
+    # simple character based spitting.
+    parsed = parse(sentence, tagset='STTS', chunks=False, relations=False, split=True)
+
+    # No parsed result, unrecoverable exception.
+    if len(parsed) == 0:
+        raise Exception('Sentence for POS tagging parsed as multiple sentences.')
+
+    # More than one parsed result, ok, take first.
+    if len(parsed) > 1:
+        warnings.warn("Sentence for POS tagging parsed as multiple sentences.")
+
+    # Return tuples of words and POS tags from parsed result.
+    return list((word[0], word[1]) for word in parsed[0])
+
 
 if __name__ == '__main__':
     # Source cites POS tags as STTS tags http://wortschatz.uni-leipzig.de/de/download#sentiWSDownload
     source = "Die Katze liegt auf der Matte. Sie ist aus Gold gemacht."
     sentences = sentences_german(source)
     for s in sentences:
-        print("Sentence: " + s)
-        print(parse(s, tagset="STTS"))
+        print(postag_german(s))
--- german/sentiment.py
+++ german/sentiment.py
@@ -1,15 +1,78 @@
+# Source of sentiment analysis http://wortschatz.uni-leipzig.de/de/download#sentiWSDownload
+import itertools
 from csv import reader
 
-# Source of sentiment analysis http://wortschatz.uni-leipzig.de/de/download#sentiWSDownload
+from german.postag import postag_german
+from german.punkt import sentences_german
+
+pos_file = 'SentiWS_v1.8c_Positive.txt'
+neg_file = 'SentiWS_v1.8c_Negative.txt'
+
+
+def load_sentiment(file):
+    """
+    Loads the sentiment file and processes it. The given sentiment files have ADJX as a short form for both ADJA and
+    ADJD, which is lifted. The scores are assigned to the base forms and all alternative forms as well. They are mapped
+    to lower case.
+    :param file: The file to load from, either pos_file or neg_file.
+    :return: Returns the sentiment data as as a dictionary (lower case word, POS) to score.
+    """
+
+    def primary(row):
+        return row[0].split('|')[0]
+
+    def poss(row):
+        pos = row[0].split('|')[1]
+        return ["ADJA", "ADJD"] if pos == "ADJX" else [pos]
+
+    def score(row):
+        return float(row[1])
+
+    def alternative(row):
+        return [] if len(row) <= 2 else row[2].split(',')
+
+    def forms(row):
+        return itertools.chain([primary(row)], alternative(row))
+
+    with open(file, 'r', encoding="utf-8") as h:
+        rows = reader(h, delimiter='\t')
+        return dict(((form.lower(), pos), score(row)) for row in rows for form in forms(row) for pos in poss(row))
+
+
+# Load individual files
+pos = load_sentiment(pos_file)
+neg = load_sentiment(neg_file)
+
+# Combine dictionaries
+combined = pos.copy()
+combined.update(neg)
+
+
+def sentiment_german(sentence_tagged):
+    """
+    Generates a score for the given sentence.
+    :param sentence_tagged: A list of words and their pos tags, as (word, POS)
+    :return: Returns the score values as (sum, n, min, max)
+    """
+    # Get scores base on word and POS tag.
+    scores = list(combined.get((word.lower(), tag), 0.0) for (word, tag) in sentence_tagged)
+
+    # Return sum, word count, min and max.
+    return sum(scores), len(scores), min(scores), max(scores)
+
+
 if __name__ == '__main__':
-    with open('SentiWS_v1.8c_Negative.txt', 'r', encoding="utf-8") as csvfile:
-        sentiment = reader(csvfile, delimiter='\t')
-        for row in sentiment:
-            primary, pos = row[0].split('|')
-            score = float(row[1])
-            alternatives = row[2].split(',') if len(row) > 2 else list()
-            print("Primary " + primary)
-            print("POS " + pos)
-            print("Score " + str(score))
-            print("Alternatives " + "; ".join(alternatives))
+    with open("../articles/articles-csv/part-00071-41da5c05-2d0c-46f7-8914-cc50230d2634-c000.csv", "r",
+              encoding="utf-8") as h:
+        articles = reader(h, doublequote=False, escapechar="\\")
+        next(articles, None)
+        for article in articles:
+            print("======================================")
+            total = 0
+            for sentence in sentences_german(article[0]):
+                tagged = postag_german(sentence)
+                analysed = sentiment_german(tagged)
+                print("%f: %s" % (analysed[0], sentence))
+                total += analysed[0]
+            print("Overall: %f" % total)
             print()
